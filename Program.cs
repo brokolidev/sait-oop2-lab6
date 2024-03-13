7b5423833832c018caf58fada24b6f721a5dd86e
Hyunseung Choi <bocalist@gmail.com>
2024-03-13 13:58:01 -0600

    serializing list and save it into a file

--- Program.cs
+++ Program.cs
@@ -26,6 +26,54 @@ namespace Lab6
             File.WriteAllText(txtPath, serializedEvent);
 
 
+            // deserialize json object from the file
+            string serializedEventFromFile = File.ReadAllText(txtPath);
+            Event deserializedEvent = JsonSerializer.Deserialize<Event>(serializedEventFromFile);
+
+            // print out information of deserialized event object
+            Console.WriteLine(deserializedEvent);
+
+
+            // create a list that contains 4 event objects
+            List<Event> events = [new Event()
+            {
+                EventNumber = 2,
+                Location = "Seoul",
+            },
+            new Event()
+            {
+                EventNumber = 3,
+                Location = "Edmonton",
+            },
+            new Event()
+            {
+                EventNumber = 4,
+                Location = "Toronto",
+            },
+            new Event()
+            {
+                EventNumber = 5,
+                Location = "Vancouver",
+            }];
+
+
+            // serialize events list itself
+            string serializedEventsList = JsonSerializer.Serialize(events);
+
+            // set the json file path
+            string jsonPath = $"{rootPath}/events.json";
+            File.WriteAllText(jsonPath, serializedEventsList);
+
+            // get text from the event.json
+            string jsonObject = File.ReadAllText(jsonPath);
+            List<Event> eventsFromJson = JsonSerializer.Deserialize<List<Event>>(jsonObject);
+
+            // print out parsed events list
+            Console.WriteLine("\nObject in the parsed events list :");
+            foreach (var item in eventsFromJson)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
