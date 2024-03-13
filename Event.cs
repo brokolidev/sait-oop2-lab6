278d74b123e3e69d873755b8b6584f3b6dfeb727
Hyunseung Choi <bocalist@gmail.com>
2024-03-13 13:39:52 -0600

    created Event class and its properties

---
+++ Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Event
+    {
+
+        public int EventNumber { get; set; }
+        public string Location { get; set; }
+
+        public Event()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Number of this event : {EventNumber} / Location : {Location}";
+        }
+    }
+}
